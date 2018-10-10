563667f2c4f8972099ff030cbbeea19a27358341
Paul Bergamo <porlybe@gmail.com>
2018-10-10 14:50:30 -0700

    Update readme Add react-bootstrap Add bootstrap sample data to index page

---
+++ src/data/store.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { createStore } from 'redux'
+
+
+
+export default createStore()
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+import { Grid, Col, Row } from 'react-bootstrap'
 
-class Home extends Component{
-    render(){
-        return <div>This is home page</div>
+
+class Home extends Component {
+    render() {
+        return (
+            <Grid>
+                <Row className="show-grid">
+                    <Col xs={12} md={8}>
+                        <code>{'<Col xs={12} md={8} />'};</code>
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <code>{'<Col xs={6} md={4} />'}</code>
+                    </Col>
+                </Row>
+
+                <Row className="show-grid">
+                    <Col xs={6} md={4}>
+                        <code>{'<Col xs={6} md={4} />'}</code>
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <code>{'<Col xs={6} md={4} />'}</code>
+                    </Col>
+                    <Col xsHidden md={4}>
+                        <code>{'<Col xsHidden md={4} />'}</code>
+                    </Col>
+                </Row>
+
+                <Row className="show-grid">
+                    <Col xs={6} xsOffset={6}>
+                        <code>{'<Col xs={6} xsOffset={6} />'}</code>
+                    </Col>
+                </Row>
+
+                <Row className="show-grid">
+                    <Col md={6} mdPush={6}>
+                        <code>{'<Col md={6} mdPush={6} />'}</code>
+                    </Col>
+                    <Col md={6} mdPull={6}>
+                        <code>{'<Col md={6} mdPull={6} />'}</code>
+                    </Col>
+                </Row>
+            </Grid>
+        )
     }
 }
 
